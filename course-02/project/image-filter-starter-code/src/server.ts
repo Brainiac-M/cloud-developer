6a95853a13d0a9f3945b062d2f51d6dd3e12e32a
Fox_Coder508 <49205684+Brainiac-M@users.noreply.github.com>
2022-06-20 00:41:23 +0100

    installed node modules but scripts still don't run

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -29,6 +29,25 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
+  app.get( "/filteredimage/", async (req:express.Request, res:express.Response) => {
+    // extract image_url from request query
+    const { image_url} = req.query;
+      if(!image_url){
+        return res.status(400).send("image_url is missing")
+      }
+      try {
+    // call filterImageFromURL(image_url) to filter the image and https://sourceforge.net/projects/unxutils/
+        const imagePath= await filterImageFromURL( "image_url" )
+        return res.status(200).sendFile(imagePath, async (error)=>{
+          await deleteLocalFiles ([imagePath])
+        });
+          } catch (error) {
+    // send the resulting file in the response
+        return res.status(422).send("image url cannot be processed")
+      }
+    } );
+
+
   //! END @TODO1
   
   // Root Endpoint
